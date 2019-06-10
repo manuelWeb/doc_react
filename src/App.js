5923237e6741561c509b46ab9764c79b809b92fe
Becquet <mdevries@domoti.fr>
2019-06-10 12:47:58 +0200

    fetch solution to import md files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 import './App.css'
 import Link from './Linktomd'
+import myMarkdownFile from './grids.md'
 
 const ReactMarkdown = require('react-markdown')
 // const testmd = require('./grids.md')
 const input = '# Header…\n\npis un paragraphe'
 
+fetch(myMarkdownFile)
+  .then(response => response.text())
+  .then(text => {
+    // Logs a string of Markdown content.
+    // Now you could use e.g. <rexxars/react-markdown> to render it.
+    console.log(text)
+  })
 class App extends Component {
-  handleLinkClick(link){
+  handleLinkClick (link) {
     console.log(link, 'clicked')
   }
   render () {
