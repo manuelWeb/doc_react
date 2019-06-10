495c1ba56b0bddae617859b96a9f15d07b3ede0d
Becquet <bq_sketch_sharelib>
2019-06-10 20:29:11 +0200

    passer la réponse txt de hopla dans le state

--- src/App.js
+++ src/App.js
@@ -3,25 +3,31 @@ import ReactMarkdown from 'react-markdown'
 import './App.css'
 import './css/github.css'
 import Link from './Linktomd'
-import source from './md_files/grids.md'
-const markdownContext = require.context('./md_files', false, /\.md$/);
+// import source from './md_files/grids.md'
+const markdownContext = require.context('./md_files', false, /\.md$/)
 console.log(markdownContext)
 console.log(markdownContext.keys())
 console.log(markdownContext.keys()[0])
 
 // solution via article medium : https://medium.com/@shawnstern/importing-multiple-markdown-files-into-a-react-component-with-webpack-7548559fce6f
-function hopla(mdname){
+function hopla(mdname) {
   // const file = 'atomic_design_systeme_SK.md'
   const readmePath = require(`./md_files/${mdname}`)
   fetch(readmePath)
     .then(response => response.text())
-    .then(data => console.log(data) )
-    // .then(data => this.setState({ data }))
+    .then(data => console.log(data))
+    .then(console.log('Ok!!!!!!!!!!!!'))
+  // .then(data => this.setState({ data }))
 }
 
 class App extends Component {
   state = {
     post: null,
+    mdtxt: "je suis mdtxt pour l'instant",
+  }
+  constructor(props) {
+    super(props)
+    this.hopla = hopla.bind(this)
   }
   // componentDidMount() {
   //   fetch(source)
@@ -29,14 +35,23 @@ class App extends Component {
   //     .then(post => this.setState((state) => ({ ...state, post })))
   //     .catch((err) => console.error(err));
   // }
-  handleLinkClick (link) {
-    console.log(link, 'clicked');
+  testfn({ ...state }) {
+    // const { mdtxt } = this.state
+    // mdtxt = 'klgjlfjdsjmrgjtjzo^yjtr'
+    // return this.state.mdtxt
+    // this.setState({ mdtxt })
+    console.log(state.mdtxt)
+  }
+  handleLinkClick(link) {
+    console.log(link, 'clicked')
     hopla(link)
   }
-  render () {
-    const { post } = this.state;
+  render() {
+    const { post, mdtxt } = this.state
+    console.log(mdtxt)
     return (
       <div className="container">
+        <button onClick={this.testfn}>Test State show clg</button>
         <Link onClick={this.handleLinkClick} />
         <div className="markdown-body">
           <ReactMarkdown source={post} escapeHtml={false} />
