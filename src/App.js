821067e359a25dd75fc1e42e48902a1fd991fce9
Becquet <mdevries@domoti.fr>
2019-06-10 13:49:42 +0200

    return fetch seems to be functional

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
 import './App.css'
 import Link from './Linktomd'
-import myMarkdownFile from './grids.md'
+// import myMarkdownFile from './md_files/grids.md'
 
 const ReactMarkdown = require('react-markdown')
 // const testmd = require('./grids.md')
 const input = '# Header…\n\npis un paragraphe'
 
-fetch(myMarkdownFile)
+function getMdByName(name) {
+  // return require("./md_files/grids.md")
+  return require(`./md_files/${name}`)
+}
+
+// const readmePath = require("./md_files/grids.md");
+// const readmePath = getMdByName("grids.md")
+var hellomd = () => fetch( getMdByName("grids.md") )
   .then(response => response.text())
   .then(text => {
-    // Logs a string of Markdown content.
-    // Now you could use e.g. <rexxars/react-markdown> to render it.
     console.log(text)
   })
+console.log(hellomd());
 class App extends Component {
   handleLinkClick (link) {
     console.log(link, 'clicked')
