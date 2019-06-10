7724227b1b339c396d6778775f5ebee12a8cb748
Becquet <mdevries@domoti.fr>
2019-06-10 12:03:35 +0200

    FIX onClick on link to log futur action on link

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css'
 import Link from './Linktomd'
 
 class App extends Component {
+  handleLinkClick(link){
+    console.log(`link:${link}`);
+  }
   render () {
     return (
       <div>
         <h1>Header</h1>
-        <Link />
+        <Link onClick={this.handleLinkClick} />
       </div>
     )
   }
