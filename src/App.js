b1538e30eec1d39f99a3a6b395ea2b628bd9fa46
Becquet <mdevries@domoti.fr>
2019-06-10 18:09:27 +0200

    work on FETCH with dynamic file

--- src/App.js
+++ src/App.js
@@ -7,22 +7,31 @@ import source from './md_files/grids.md'
 const markdownContext = require.context('./md_files', false, /\.md$/);
 console.log(markdownContext)
 console.log(markdownContext.keys())
+console.log(markdownContext.keys()[0])
 
 // solution via article medium : https://medium.com/@shawnstern/importing-multiple-markdown-files-into-a-react-component-with-webpack-7548559fce6f
-
+function hopla(mdname){
+  // const file = 'atomic_design_systeme_SK.md'
+  const readmePath = require(`./md_files/${mdname}`)
+  fetch(readmePath)
+    .then(response => response.text())
+    .then(data => console.log(data) )
+    // .then(data => this.setState({ data }))
+}
 
 class App extends Component {
   state = {
     post: null,
   }
-  componentDidMount() {
-    fetch(source)
-      .then(res => res.text())
-      .then(post => this.setState((state) => ({ ...state, post })))
-      .catch((err) => console.error(err));
-  }
+  // componentDidMount() {
+  //   fetch(source)
+  //     .then(res => res.text())
+  //     .then(post => this.setState((state) => ({ ...state, post })))
+  //     .catch((err) => console.error(err));
+  // }
   handleLinkClick (link) {
-    console.log(link, 'clicked')
+    console.log(link, 'clicked');
+    hopla('grids.md')
   }
   render () {
     const { post } = this.state;
