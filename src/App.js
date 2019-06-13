52ab5971caa4a7d2893f81c9bc758fe6468dfd48
Manueldevries <manuel@webdesignord.fr>
2019-06-13 15:23:54 +0200

    MV src/css/*.css to src/assets/styles/*.scss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 import ReactMarkdown from 'react-markdown'
-import './css/App.css'
-import './css/github.css'
+import './assets/styles/App.scss'
+import './assets/styles/github.scss'
 import Link from './Linktomd'
 const markdownContext = require.context('./md_files', false, /\.md$/);
 console.log(markdownContext)
 console.log(markdownContext.keys())
-
-// solution via article medium : https://medium.com/@shawnstern/importing-multiple-markdown-files-into-a-react-component-with-webpack-7548559fce6f
-
 
 class App extends Component {
   constructor(props){
@@ -20,7 +17,7 @@ class App extends Component {
   }
   load() {
     setTimeout(() => {
-      fetch('https://api.aladhan.com/timingsByCity?city=france&country=US&method=2')
+      fetch('https://api.aladhan.com/timingsByCity?city=usa&country=US&method=2')
         .then(res => res.text())
         .then(post => this.setState((state) => ({ ...state, post })))
         .catch((err) => console.error(err));
@@ -28,7 +25,7 @@ class App extends Component {
   }
   handleLinkClick(link) {
     console.log(`./md_file/${link}`, 'clicked', 'this:', this)
-    fetch(require(`./md_files/${link}`))
+    fetch(`./md_files/${link}`)
       .then(res => res.text())
       .then(md => this.setState({ post: md }) )
       .then(md => console.log(md) )
@@ -37,7 +34,8 @@ class App extends Component {
     const { post } = this.state;
     return (
       <div className="container">
-        <Link onClick={ (i) => {this.handleLinkClick(i);this.load()} } />
+        {/* <Link onClick={ (i) => {this.handleLinkClick(i);this.load()} } /> */}
+        <Link onClick={ (i) => {this.handleLinkClick(i)} } />
         <div className="markdown-body">
           <ReactMarkdown source={post} escapeHtml={false} />
         </div>
