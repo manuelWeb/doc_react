c88813a4f39e1b93b11a9ea1befaad2361a2d137
Manueldevries <manuel@webdesignord.fr>
2019-06-21 16:12:51 +0200

    study BEM method

--- src/Linktomd.js
+++ src/Linktomd.js
@@ -1,4 +1,13 @@
 import React from 'react'
+
+const listLink = [
+  { id: 1, text: 'Atomic Design Method', link: 'atomic_design_systeme_SK.md' },
+  { id: 2, text: 'Atomic Design exemples', link: 'atomic_design_exemples.md' },
+  { id: 4, text: 'Grids out of date ', link: 'grids.md' },
+  { id: 5, text: '8pt Grid system', link: 'heightPtGridSys.md' },
+  { id: 6, text: 'B__E--M', link: 'bem.md' },
+]
+
 const Emoji = props => (
   <span
       className="emoji"
@@ -11,31 +20,42 @@ const Emoji = props => (
 
 );
 
-const listLink = [
-  { id: 1, text: 'Atomic Design Method', link: 'atomic_design_systeme_SK.md' },
-  { id: 2, text: 'Atomic Design exemples', link: 'atomic_design_exemples.md' },
-  { id: 4, text: 'Grids out of date ', link: 'grids.md' },
-  { id: 5, text: '8pt Grid system', link: 'heightPtGridSys.md' },
-  { id: 6, text: 'BEM', link: 'bem.md' },
-]
-// const Link = ({ onClick, e, isActif }) => {
+const SvgFb = props => (
+  <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+    <g fillRule="nonzero" fill="none">
+      <path
+        d="M5 0h14a5 5 0 0 1 5 5v14a5 5 0 0 1-5 5H5a5 5 0 0 1-5-5V5a5 5 0 0 1 5-5z"
+        fill="#BFBEC0"
+      />
+      <path
+        d="M9.506 22h3.735l.002-10 3.393-.001L17 8.552h-3.759l.005-1.724c0-.9.061-1.38 1.347-1.38h2.375V2h-3.406c-3.3 0-4.06 1.711-4.06 4.522l.004 2.03L7 8.555V12h2.506v10z"
+        fill="#FFF"
+      />
+    </g>
+  </svg>
+);
+
 const Link = ({ onClick, e, id }) => {
   return (
-    <ul>
-      {listLink.map(item => (
-        <li key={item.id}>
-          <a
-            className={id === item.id ? 'actif' : ''}
-            href={`#filename:~${item.link}/`}
-            onClick={e => {
-              onClick(item.link, e, item.id)
-            }}
-          >
-            {item.text}{item.id === 4 && <Emoji symbol="🙁" />}
-          </a>
-        </li>
-      ))}
-    </ul>
+    <nav className="nav-block">
+      <ul className="items-list">
+        {listLink.map(item => (
+          <li key={item.id} className="items-list__item">
+            <a
+              className={id === item.id ? 'actif' : ''}
+              href={`#filename:~${item.link}/`}
+              onClick={e => {
+                onClick(item.link, e, item.id)
+              }}
+            >
+              {item.text}
+              {item.id === 4 && <Emoji symbol="😅" />}
+              {item.id === 2 && <SvgFb className="svg--svg-baseline" />}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
