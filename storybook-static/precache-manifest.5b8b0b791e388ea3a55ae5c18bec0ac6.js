ef97bf166c74af6664135276ae2fa6549d2bec88
Becquet <mdevries@domoti.fr>
2019-06-28 19:21:23 +0200

    TEST storybook-build on github pages

---
+++ storybook-static/precache-manifest.5b8b0b791e388ea3a55ae5c18bec0ac6.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9f321af1a075f1b664f5e2d7010edf",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.39320e609188fe5b68fa.bundle.js"
+  },
+  {
+    "revision": "8c0161603a7b8ba968fd",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.39320e609188fe5b68fa.bundle.js"
+  },
+  {
+    "url": "vendors~main.39320e609188fe5b68fa.bundle.js"
+  }
+]);
