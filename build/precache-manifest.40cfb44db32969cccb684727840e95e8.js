dd6926609cf93f57a68236bae066c889ca599360
Manueldevries <manuel@webdesignord.fr>
2019-06-28 14:47:22 +0200

    Build app to test on github pages 🤞

---
+++ build/precache-manifest.40cfb44db32969cccb684727840e95e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b554f01234ec9d7171592bb99a61ddd6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "529bb3076fe309fa922f",
+    "url": "./static/css/main.12f049eb.chunk.css"
+  },
+  {
+    "revision": "a685e234beffcd3cfcb3",
+    "url": "./static/js/2.53bf7888.chunk.js"
+  },
+  {
+    "revision": "529bb3076fe309fa922f",
+    "url": "./static/js/main.e909418a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
