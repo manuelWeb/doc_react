402fc4f93e84b872547fc8d664a8b19ee8d312fb
Manueldevries <manuel@webdesignord.fr>
2019-07-10 17:47:11 +0200

    UPDATE doc_react

--- web_project_doc/gh-pages/precache-manifest.3ef77fab1aae46abd853549b5690efe6.js
+++ web_project_doc/gh-pages/precache-manifest.3ef77fab1aae46abd853549b5690efe6.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b554f01234ec9d7171592bb99a61ddd6",
+    "revision": "6ec7d27af076becb736462475ab3a21d",
     "url": "./index.html"
   },
   {
-    "revision": "529bb3076fe309fa922f",
-    "url": "./static/css/main.12f049eb.chunk.css"
+    "revision": "84611f7a1f00a39e28ca",
+    "url": "./static/css/main.fd6342d1.chunk.css"
   },
   {
     "revision": "a685e234beffcd3cfcb3",
     "url": "./static/js/2.53bf7888.chunk.js"
   },
   {
-    "revision": "529bb3076fe309fa922f",
-    "url": "./static/js/main.e909418a.chunk.js"
+    "revision": "84611f7a1f00a39e28ca",
+    "url": "./static/js/main.22900f3b.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
--- web_project_doc/gh-pages/service-worker.js
+++ web_project_doc/gh-pages/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.40cfb44db32969cccb684727840e95e8.js"
+  "./precache-manifest.3ef77fab1aae46abd853549b5690efe6.js"
 );
 
 self.addEventListener('message', (event) => {
